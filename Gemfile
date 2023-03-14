1b2e7525ede461551bf4bc6655ed7ee1cc881dc3
Jon Sangster <jon@ertt.ca>
2023-03-14 00:59:07 -0700

    Add simplecov dev dependency

--- Gemfile
+++ Gemfile
@@ -12,5 +12,6 @@ group :development do
   gem 'rubocop', '~> 1.45'
   gem 'rubocop-rake', '~> 0.6'
   gem 'rubocop-rspec', '~> 2.18'
+  gem 'simplecov', '~> 0.22'
   gem 'yard', '~> 0.9'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+unless ENV.key?('SKIP_COVERAGE')
+  require 'simplecov'
+  Pathname(__dir__).join('../coverage').tap { _1.rmtree if _1.directory? }
+  SimpleCov.start do
+    enable_coverage :branch
+    primary_coverage :branch
+    add_filter %r{^/spec/}
+  end
+end
+
 require 'bundix'
 require 'pry-byebug'
 Dir[Pathname.new(__dir__).join('support', '**', '*.rb')].sort.each { |f| require f }
